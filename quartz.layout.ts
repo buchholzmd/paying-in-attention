a417c8580c1c743029d4e43514351a153b566c0e
buchholzmd <buchholz.md@gmail.com>
2024-08-09 00:18:17 -0400

    Quartz sync: Aug 9, 2024, 12:18 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -44,7 +44,8 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
+    // Component.DesktopOnly(),
   ],
   right: [],
 }
