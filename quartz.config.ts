8f51201538a752e754b74cc9aa9f7d825ddc0be2
buchholzmd <buchholz.md@gmail.com>
2024-08-09 10:57:41 -0400

    Quartz sync: Aug 9, 2024, 10:57 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     baseUrl: "buchholzmd.github.io/paying-in-attention",
     ignorePatterns: [
       "private", 
-      "content/Assets",
+      "../content/Assets",
     ],
     defaultDateType: "created",
     theme: {
